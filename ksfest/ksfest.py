dd5073b6600422a2c112b7f9407da739cc411a1a
oristides <oristides@gmail.com>
2019-11-26 22:19:16 -0300

    function update

--- ksfest/ksfest.py
+++ ksfest/ksfest.py
@@ -1,3 +1,5 @@
+# Any results you write to the current directory are saved as output.
+
 # -*- coding: utf-8 -*-
 
 import numpy as np # linear algebra
@@ -10,10 +12,11 @@ from tqdm import tqdm
 class ks_fest(object):
     def __init__(self):
         
-        self.dict_ks = dict()
+        #self.dict_ks = dict()
         self.dict_cdfs_var_dim=dict()
-
-    def fit_curves(self, df, var_dim, resolution=500,sample=0.05):
+        self.cols=None
+        
+    def fit_curves(self, df, var_dim, columns=None, resolution=500,sample=0.10,):
 
 
         """
@@ -41,10 +44,13 @@ class ks_fest(object):
         if df.shape[0]<resolution:  
             resolution=df.shape[0]
         
+        if columns==None:
+            columns=[col for col in list(df.columns) if col!=var_dim]
+        
 
         for dim in np.unique(df[var_dim]):
             dict_cdfs={}
-            for col in df.columns:    
+            for col in columns:    
                 sorted_col=np.sort(df.loc[df[var_dim]==dim, col].fillna(-1))
                 n=len(sorted_col)
                 bins_vector=np.linspace(start=min(sorted_col),stop=max(sorted_col),num=resolution)
@@ -52,16 +58,13 @@ class ks_fest(object):
                 dict_cdfs[col]=cdf
             self.dict_cdfs_var_dim[dim]=dict_cdfs
 
-        else:
-
-            for col in df.columns:    
-                    sorted_col=np.sort(df.loc[df[var_dim]==dim, col].fillna(-1))
-                    n=len(sorted_col)
-                    bins_vector=np.linspace(start=min(sorted_col),stop=max(sorted_col),num=resolution)
-                    cdf= np.searchsorted(bins_vector, sorted_col)/n
-                    self.dict_cdfs_var_dim[col]=cdf
-
-
+        #else:
+        #    for col in columns:    
+        #            sorted_col=np.sort(df.loc[df[var_dim]==dim, col].fillna(-1))
+        #            n=len(sorted_col)
+        #            bins_vector=np.linspace(start=min(sorted_col),stop=max(sorted_col),num=resolution)
+        #            cdf= np.searchsorted(bins_vector, sorted_col)/n
+        #            self.dict_cdfs_var_dim[col]=cdf
 
     def fit_ks(self, df,var_dim,columns, sample):
         for comb in tqdm(itertools.combinations(np.unique(df[var_dim]),2)):
@@ -80,7 +83,27 @@ class ks_fest(object):
             self.pandas_ks.columns=[col for col in df.columns if col!=var_dim]
             self.pandas_ks['safra']=pandas_ks.index
             self.pandas_ks.index=range(len(pandas_ks))
+    
+    
+    def fit_ks_curves(self,df,var_dim,columns, sample):
+        for comb in tqdm(itertools.combinations(np.unique(df[var_dim]),2)):
+            ks_list=[]
+            
+            if columns==None:
+                columns=df.columns
 
+            for col in [col for col in columns if col!=var_dim]:
+                ks_list.append(ks_2samp(df.loc[df[var_dim]==comb[0], col].sample(frac=0.1).fillna(-1), df.loc[df[var_dim]==comb[1], col].sample(frac=0.1).fillna(-1))[0])
+            self.dict_ks[str(comb[0])+'_'+str(comb[1])] = ks_list
+            
+
+            pandas_ks_=pd.DataFrame().from_dict(self.dict_ks)
+            self.pandas_ks= pandas_ks_.T
+            self.pandas_ks.columns=[col for col in df.columns if col!=var_dim]
+            self.pandas_ks['safra']=pandas_ks.index
+            self.pandas_ks.index=range(len(pandas_ks))
+
+    
     #def save_cdfs(fname, self.disct_cdfs):
     #    pickle.dump(fname,'wd')
         
