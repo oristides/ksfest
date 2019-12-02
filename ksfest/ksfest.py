07b1269b09981764eba385fca233a83dbfdbabd3
oristides <oristides@gmail.com>
2019-12-02 19:02:18 -0300

    functional fit_ks_curves

--- ksfest/ksfest.py
+++ ksfest/ksfest.py
@@ -1,12 +1,6 @@
-# Any results you write to the current directory are saved as output.
-
-# -*- coding: utf-8 -*-
-
-import numpy as np # linear algebra
-import pandas as pd 
 import itertools
 from scipy.stats import ks_2samp
-from tqdm import tqdm 
+from tqdm import tqdm
 #import pickle
 
 class ks_fest(object):
@@ -15,6 +9,7 @@ class ks_fest(object):
         #self.dict_ks = dict()
         self.dict_cdfs_var_dim=dict()
         self.cols=None
+        self.dict_ks=dict()
         
     def fit_curves(self, df, var_dim, columns=None, resolution=500,sample=0.10,):
 
@@ -86,23 +81,40 @@ class ks_fest(object):
     
     
     def fit_ks_curves(self,df,var_dim,columns, sample):
+        """
+
+        Fit and save cdf to check data quality.
+
+
+        Parameters
+        ----------
+        df : pandas dataframe
+        var_dim : string 
+        sample: samplin portion from datafram
+        
+        Attributes
+        ----------
+        """
+
+    
+        if columns==None:
+                columns=[col for col in columns if col!=var_dim]
+        
+        
         for comb in tqdm(itertools.combinations(np.unique(df[var_dim]),2)):
             ks_list=[]
             
-            if columns==None:
-                columns=df.columns
-
-            for col in [col for col in columns if col!=var_dim]:
-                ks_list.append(ks_2samp(df.loc[df[var_dim]==comb[0], col].sample(frac=0.1).fillna(-1), df.loc[df[var_dim]==comb[1], col].sample(frac=0.1).fillna(-1))[0])
+            for col in columns:
+                ks_list.append(ks_2samp(df.loc[df[var_dim]==comb[0], col].sample(frac=sample).fillna(-1), df.loc[df[var_dim]==comb[1], col].sample(frac=sample).fillna(-1))[0])
             self.dict_ks[str(comb[0])+'_'+str(comb[1])] = ks_list
             
 
-            pandas_ks_=pd.DataFrame().from_dict(self.dict_ks)
-            self.pandas_ks= pandas_ks_.T
-            self.pandas_ks.columns=[col for col in df.columns if col!=var_dim]
-            self.pandas_ks['safra']=pandas_ks.index
-            self.pandas_ks.index=range(len(pandas_ks))
-
+        pandas_ks_=pd.DataFrame().from_dict(self.dict_ks)
+        self.pandas_ks= pandas_ks_.T
+        self.pandas_ks.columns=columns
+        self.pandas_ks[var_dim]=self.pandas_ks.index
+        self.pandas_ks.index=range(len(self.pandas_ks))
+        return self.pandas_ks
     
     #def save_cdfs(fname, self.disct_cdfs):
     #    pickle.dump(fname,'wd')
