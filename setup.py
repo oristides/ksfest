41977995604b40862c1b487aecc947fc4bb79ed6
oristides <oristides@gmail.com>
2019-12-04 23:03:58 -0300

    update test4

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Natural Language basestring:: English',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
