a8416c2d33e3fd96c41baff1fb2a4be6d1fd3eef
oristides <oristides@gmail.com>
2021-09-13 21:49:26 -0300

    save and load method

--- ksfest/ksfest.py
+++ ksfest/ksfest.py
@@ -4,68 +4,88 @@ from tqdm import tqdm
 import numpy as np
 import pandas as pd
 from .utils import read_sample
+import pickle
+
 
 class ks_fest(object):
 
     def __init__(self):
 
-        self.cols=None
-        self.dict_ks=dict()
-        self.dict_ks_pvalues=dict()
-    
-    def get_ks(self,df,var_dim, sample,columns=None, na_number=-1, **kwargs):
+        self.cols = None
+        self.dict_ks = dict()
+        self.dict_ks_pvalues = dict()
+        self.iter_combs = None
 
+    def get_ks(self, df, var_dim, sample, columns=None, na_number=-1, **kwargs):
         """
 
-        Fit and save cdf to check data quality.
+        Fit cdf to check data quality.
 
         Parameters
         ----------
         df: pandas dataframe or csv file
         var_dim: string 
-        
-        sample: samplin portion from dataframe
+
+        sample: sampling portion from dataframe
         columsn: list of columsn to calculate ks
         na_number: default number if data is missing
         Attributes
         ----------
-        
+
         """
 
         if not isinstance(df, pd.DataFrame):
-            df=read_sample(df, sample_size=sample, **kwargs)
+            try:
+                df = read_sample(df, sample_size=sample, **kwargs)
 
-        if columns==None:
-                columns=[col for col in df.columns if col!=var_dim]
-                self.cols=columns        
+        if columns == None:
+            columns = [col for col in df.columns if col != var_dim]
+            self.cols = columns
         
-        #Teste
+        if not isinstance(columns, list) and not isinstance(columns, np.ndarray):
+            raise ValueError("Columns must be a list or a numpy dataframe os strings")
+        else:
+            columns =  [col for col in columns if col != var_dim]
+            self.cols = columns
 
-        if not isinstance(columns,list) and not isinstance(columns, np.ndarray):
-            raise ValueError("Columns must be a list or a numpy dataframe os strings") 
-
-        try:
-            all(df.dtypes.values==float)
-        except:
+        if not all(df.dtypes.values == float):
             raise TypeError("Only numeric columns are allowed")
 
-        for comb in tqdm(itertools.combinations(np.unique(df[var_dim]),2)):
-            ks_list=[]
-            pvalue_list=[]
+        #Ground Zero
+        if self.iter_combs == None:
+            self.iter_combs = itertools.combinations(np.unique(df[var_dim]), 2)
 
+
+        for comb in tqdm(self.iter_combs):
+            ks_list = []
+            pvalue_list = []
 
             for col in columns:
-                ks_result=ks_2samp(df.loc[df[var_dim]==comb[0], col].sample(frac=sample).fillna(na_number), df.loc[df[var_dim]==comb[1], col].sample(frac=sample).fillna(na_number))
+                ks_result = ks_2samp(df.loc[df[var_dim] == comb[0], col].sample(frac=sample).fillna(
+                    na_number), df.loc[df[var_dim] == comb[1], col].sample(frac=sample).fillna(na_number))
                 ks_list.append(ks_result[0])
                 pvalue_list.append(ks_result[1])
 
-            self.dict_ks[str(comb[0])+'_'+str(comb[1])] = ks_list
-            self.dict_ks_pvalues[str(comb[0])+'_'+str(comb[1])] = pvalue_list
+            self.dict_ks[str(comb[0]) + '_' + str(comb[1])] = ks_list
+            self.dict_ks_pvalues[str(comb[0]) + '_' +
+                                 str(comb[1])] = pvalue_list
 
-        pandas_ks_=pd.DataFrame().from_dict(self.dict_ks)
-        self.pandas_ks= pandas_ks_.T
-        self.pandas_ks.columns=columns
-        self.pandas_ks[var_dim]=self.pandas_ks.index
-        self.pandas_ks.index=range(len(self.pandas_ks))
+        pandas_ks_ = pd.DataFrame().from_dict(self.dict_ks)
+        self.pandas_ks = pandas_ks_.T
+        self.pandas_ks.columns = columns
+        self.pandas_ks[var_dim] = self.pandas_ks.index
+        self.pandas_ks.index = range(len(self.pandas_ks))
         return self.pandas_ks
-    
+
+        def save(self, filename):
+            f=open(filename, 'wb')
+            pickle.dump(self.__dict__, f, pickle.HIGHEST_PROTOCOL)
+            f.close()
+
+
+        def load(self, model_file):
+            f = open(self.filename, 'rb')
+            tmp_dict = cPickle.load(f)
+            f.close()
+            self.__dict__.clear()
+            self.__dict__.update(tmp_dict)
--- tests/test_ksfest.py
+++ tests/test_ksfest.py
@@ -2,19 +2,27 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `ksfest` package."""
+# Any changes to the distributions library should be reinstalled with
+#  pip install --upgrade .
 
+# For running unit tests, use
+# /usr/bin/python -m unittest test
 
+from .ksfest.ksfest import ks_fest
 import unittest
-import click
-from click.testing import CliRunner
 
 
 class TestKsfest(unittest.TestCase):
     """Tests for `ksfest` package."""
 
-
     def setUp(self):
         """Set up test fixtures, if any."""
+        self.ksf = ks_fest()
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
+
+    def test_initialization(self):
+        self.assertEqual(self.gaussian.mean, 25, 'incorrect mean')
+        self.assertEqual(self.gaussian.stdev, 2,
+                         'incorrect standard deviation')
